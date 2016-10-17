e95d1d94a2e286577cf0fe83487747e08b30f164
hanyu <360548055@qq.com>
2016-10-17 17:12:51 +0800

    first1017 splashactivity

---
+++ mobilesafe/src/com/hanyu/mobilesafe/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.hanyu.mobilesafe;
+
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class SplashActivity extends Activity {
+
+	private TextView mTvSpalshVersion;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_splash);
+		mTvSpalshVersion = (TextView) findViewById(R.id.tv_splash_version);
+		mTvSpalshVersion.setText("veision:" + getVersion());
+		
+	}
+
+	private String getVersion() {
+		PackageManager pm = getPackageManager();
+		try {
+			PackageInfo info = pm.getPackageInfo(getPackageName(), 0);
+			return info.versionName;
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+			return "";
+		}	
+	}
+}
